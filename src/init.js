520998da31efb0a6ec4b0236f775bd10003919dd
DSC <2586900641@qq.com>
2022-11-03 22:13:58 +0800

    通过模版构建ast树

--- src/init.js
+++ src/init.js
@@ -1,9 +1,32 @@
 import { initState } from "./initState"
+import {complieToFunction} from './complie/index.js'
 
 export function initMixin(MyVue2){
     MyVue2.prototype._init=function(options){
         const vm=this
         vm.$options=options
         initState(vm)
+        if(options.el){
+            vm.$mount(options.el)
+        }
+    }
+    MyVue2.prototype.$mount=function(el){
+       const vm=this
+       const opts=vm.$options
+       el=document.querySelector(el);
+       if(!opts.render){//优先级render>template>el
+        let template
+        if(!opts.template){
+            if(el){
+                template=el.outerHTML
+            }else{
+                console.error('模板解析失败')
+            }
+        }else{
+            template=opts.template
+        }
+        const render=complieToFunction(template)
+        opts.render=render//后两种情况最后也要有一个render
+       }
     }
 }
